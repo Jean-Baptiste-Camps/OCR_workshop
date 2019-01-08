9e196be935d70b591b245a25c7d01ffd7188ae13
Jean-Baptiste Camps <jbcamps@hotmail.com>
2019-01-08 13:55:25 +0100

    randomisation

---
+++ randomise_data.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys
+import random
+
+if __name__ == '__main__':
+
+    train = open("train.txt", "w")
+    val = open("val.txt", "w")
+    test = open("test.txt", "w")
+
+    random.seed('1214')
+
+    for i in sys.argv[1:]:
+        monRand = random.random()
+        if monRand <= 0.1:
+            test.write(i+"\n")
+        elif 0.1 < monRand <= 0.2:
+            val.write(i+"\n")
+        else:
+            train.write(i+"\n")
+
+    test.close()
+    val.close()
+    train.close()
